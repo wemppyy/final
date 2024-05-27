84e44c4344a3b9c0b6cbd65ee021ec5c6b649a9f
wemppyy <denisfrostaliuk@gmail.com>
2024-05-28 02:21:10 +0300

    first all courses + fix adaptive

---
+++ js/all_courses.js
@@ -0,0 +1,59 @@
+const burgerBtn = document.getElementById("burger-btn");
+const burgerMenu = document.getElementById("burger-menu");
+const body = document.body;
+
+burgerBtn.onclick = () => {
+  burgerMenu.classList.toggle("active");
+  body.classList.toggle("overflow");
+};
+
+const catalog = document.querySelectorAll(".catalog__row-column");
+
+const kids = document.querySelectorAll(".kid");
+const childs = document.querySelectorAll(".child");
+const adults = document.querySelectorAll(".adult");
+
+function filter(data) {
+  if (data === "kid") {
+    kids.forEach((element) => {
+      element.classList.add("active");
+    });
+    childs.forEach((element) => {
+      element.classList.remove("active");
+    });
+    adults.forEach((element) => {
+      element.classList.remove("active");
+    });
+  } else if (data === "child") {
+    kids.forEach((element) => {
+      element.classList.remove("active");
+    });
+    childs.forEach((element) => {
+      element.classList.add("active");
+    });
+    adults.forEach((element) => {
+      element.classList.remove("active");
+    });
+  } else {
+    kids.forEach((element) => {
+      element.classList.remove("active");
+    });
+    childs.forEach((element) => {
+      element.classList.remove("active");
+    });
+    adults.forEach((element) => {
+      element.classList.add("active");
+    });
+  }
+}
+
+catalog.forEach((element) => {
+  filter(element.getAttribute("data-type"));
+  element.onclick = function () {
+    catalog.forEach((i) => {
+      i.classList.remove("active");
+    });
+    element.classList.add("active");
+    filter(element.getAttribute("data-type"));
+  };
+});
