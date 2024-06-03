97ac448bb3126515ad94e3e509ae21a97c5624aa
wemppyy <denisfrostaliuk@gmail.com>
2024-06-04 00:33:26 +0300

    definitely the end

--- js/login.js
+++ js/login.js
@@ -9,12 +9,12 @@ burgerBtn.onclick = () => {
 
 names = ['Вхід', 'Реєстрація']
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   pagination: {
     clickable: true,
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + names[index] + '</span>';
+      return '<span class="' + className + '">' + names[index] + "</span>";
     },
   },
   allowTouchMove: false,
